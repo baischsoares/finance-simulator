12804473fcc48db549a26ef30299e98fcd78d23c
baischsoares <baischsoares@gmail.com>
2023-01-14 16:51:39 -0300

    header e inicio da logica para buscar acoes

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,54 @@
+<template>
+  <header>
+    <nav>
+      <div class="links">
+        <router-link to="/">Início</router-link> 
+        <router-link to="/acoes">Buscar ações</router-link>
+        <router-link to="/investimentos">Meus investimentos</router-link>
+      </div>
+        <div>
+          <img class="img-perfil" src="../assets/avatar.png" alt="">
+        </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderNav',
+}
+</script>
+
+
+<style scoped>
+header{
+  margin-bottom: 40px;
+}
+nav{
+  box-shadow: 0 4px 8px rgba(20, 40, 60, .2);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 15px;
+}
+a{
+  display: inline-block;
+  margin-right: 30px;
+  color: var(--corAzul);
+  font-weight: 600;
+  padding: 10px 20px;
+  border-radius: 4px;
+}
+a:hover {
+  background: var(--corAzul);
+  color: var(--corBranca);
+}
+.img-perfil{
+  height: 60px;
+  vertical-align: top;
+}
+.router-link-exact-active{
+  background: var(--corAzul);
+  color: var(--corBranca);
+}
+</style>
---
+++ src/views/Acoes.vue
@@ -0,0 +1,58 @@
+<template>
+ <BuscarAcoes />
+ <InformacoesAcao />
+</template>
+
+<script>
+
+import BuscarAcoes from '@/components/BuscarAcoes.vue'
+import InformacoesAcao from '@/components/InformacoesAcao.vue'
+
+
+
+export default {
+  name: "PaginaAcoes",
+  components: {
+    BuscarAcoes,
+    InformacoesAcao
+  },
+  methods:{
+    buscarAcoes(){
+      this.$router.push( {query: {q: this.acao}} )
+    }
+  },
+}
+</script>
+
+<style scoped>
+form{
+  position: relative;
+  display: inline-block;
+}
+input{
+  border-radius: 4px;
+  border: none;
+  padding: 10px;
+}
+input[type="text"]{
+  width: 300px;
+  border: 1px solid var(--corAzul);
+}
+input[type="submit"]{
+  height: 42px;
+  position: absolute;
+  right: 1px;
+  cursor: pointer;
+  background: transparent;
+}
+input[type="text"]:focus,
+input[type="text"]:hover{
+  outline: var(--corAzul);
+  border: 2px solid var(--corAzul);
+}
+input[type="submit"]:hover{
+  background: var(--corAzul);
+  color: var(--corBranca);
+}
+
+</style>
